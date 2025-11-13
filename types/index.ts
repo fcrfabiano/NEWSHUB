4f36454503bf05b8b44d5b6833c557b935bb93a5
Fabiano César <fcfabiano.cesar@gmail.com>
2025-11-13 11:31:56 -0300

    feat: implement types

---
+++ types/index.ts
@@ -0,0 +1,43 @@
+export interface Article {
+    id: string;
+    title: string;
+    description: string;
+    content: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    source: {
+        id: string | null;
+        name: string;
+    };
+    category?: string;
+    author?: string;
+}
+
+export interface Topic {
+    id: string;
+    articleId: string;
+    title: string;
+    content: string;
+    createdAt: string;
+    author: string;
+}
+
+export interface NewsApiResponse {
+    status: string;
+    totalResults: number;
+    articles: Article[];
+}
+
+export interface ArticleResponse {
+    id: number;
+    created_at: string;
+    author: string;
+    title: string;
+    url: string;
+    points: number;
+    children: string[];
+    text: string;
+    type: string;
+    parent_id: number;
+}
